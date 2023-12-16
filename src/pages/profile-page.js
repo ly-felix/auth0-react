fbdda34f950d235eca0b43833690d98ff03ac853
ly-felix <71753872+ly-felix@users.noreply.github.com>
2023-12-17 02:01:47 +0800

    a

--- src/pages/profile-page.js
+++ src/pages/profile-page.js
@@ -34,9 +34,9 @@ export const ProfilePage = () => {
                 className="profile__avatar"
               />
               <div className="profile__headline">
-                <h2 className="profile__title">{user.name}</h2>  
+                <h2 className="profile__title">{user.name}</h2>
                 <span className="profile__description">{user.email}</span>
-                </div>
+              </div>
             </div>
             <div className="profile__details">
               <CodeSnippet
