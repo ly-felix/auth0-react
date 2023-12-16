24baee73b0372e263692bfe0ddf472436dc1b876
ly-felix <71753872+ly-felix@users.noreply.github.com>
2023-12-17 01:01:33 +0800

    Update profile-page.js

--- src/pages/profile-page.js
+++ src/pages/profile-page.js
@@ -44,6 +44,10 @@ export const ProfilePage = () => {
                 code={JSON.stringify(user, null, 2)}
               />
             </div>
+            <div className="profile__headline">
+                <h2 className="profile__title">{user.name}</h2>
+                <span className="profile__description">{user.email}</span>
+            </div>
           </div>
         </div>
       </div>
