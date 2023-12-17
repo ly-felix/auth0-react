96a96a65de8c68368c56512f763e74b4ab4e6f1e
ly_f <610224301@qq.com>
2023-12-17 16:09:45 +0800

    b

--- src/pages/profile-page.js
+++ src/pages/profile-page.js
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-import { CodeSnippet } from "../components/code-snippet";
+// import { CodeSnippet } from "../components/code-snippet";
 import { PageLayout } from "../components/page-layout";
 
 export const ProfilePage = () => {
